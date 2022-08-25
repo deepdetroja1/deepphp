db6b4362455ec9d23d40775d79e043670bb814a7
jaydeep-makwana <jobandstudypoint@gmail.com>
2022-08-24 19:07:01 -0700

    first commit

---
+++ p2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // pyramid 1 :- *
+    // for ($i=1;$i<=10;$i++){
+    //     for($j=1;$j<=10-$i;$j++){
+    //         echo "&nbsp &nbsp;";
+    //     }
+    //     for($k=1;$k<=$i;$k++){
+    //         echo "* &nbsp &nbsp;";
+    //     }
+    //     echo"</br>";
+    // }
+
+    //pyramid 2 :-  1 to 15 
+    // $n =1;
+    // for($i=1;$i<=5;$i++){
+    //     for($j=1;$j<=$i;$j++){
+    //         echo $n. " ";
+    //         $n= $n +1;
+    //     }
+    //     echo "</br>";
+    // }
+
+    //pyramid 3 :- 1,12,123 ...
+    // for($i=1;$i<=5;$i++){
+    //     for($j=1;$j<=$i;$j++){
+    //         echo($j."&nbsp");
+    //     }
+    //     echo"</br>";
+    // }
+    
+    //pyramid 4 :- 5,54,543..
+    // for($i=5;$i>=1;$i--){
+    //     for($j=5;$j>=$i;$j--){
+    //         echo ($j."&nbsp;");
+    //     }
+    //     echo "</br>";
+    // }
+
+
+    //pyramid 5 :-
+
+    // ?>
+    
+</body>
+</html>
